69f8e005c63647b55bbe97666bfe06c6e65ce5f7
Rodrigo Limões <rolimoes@gmail.com>
2023-04-20 09:25:37 -0300

    feat: Adding getProductsAverage function

--- src/stats/stats.service.ts
+++ src/stats/stats.service.ts
@@ -1,14 +1,33 @@
 import { StatsService as Service } from '@stats/interface/statsService.interface';
 import { RatingService } from '@src/rating/rating.service';
+import { Repository } from 'typeorm';
+import { Rating } from '@rating/entity/rating.entity';
+import { ProductsAverage } from '@stats/interface/ProductsAverage';
 
 export class StatsService implements Service {
-	constructor(private ratingService: RatingService) {}
+	constructor(
+		private ratingService: RatingService,
+		private ratingRepository: Repository<Rating>
+	) {}
+
+	getProductsAverage = async (
+		productIds: Array<number>
+	): Promise<Array<ProductsAverage>> => {
+		return Array.isArray(productIds) && productIds.length > 0
+			? await this.ratingRepository
+					.createQueryBuilder('rating')
+					.select([
+						'rating.productId, COUNT("productId")::float as total, AVG(rating.ratingScale)::float as average',
+					])
+					.where(`rating.productId In (${productIds})`)
+					.groupBy('rating.productId')
+					.getRawMany()
+			: [];
+	};
 
 	getAverage = async (productId: number): Promise<number> => {
 		const ratings = await this.ratingService.findByProduct(productId);
-		const averageRating =
-			ratings.reduce((a, b) => a + b.ratingScale, 0) / ratings.length || 0;
-		return averageRating;
+		return ratings.reduce((a, b) => a + b.ratingScale, 0) / ratings.length || 0;
 	};
 
 	getPercentage = async (
