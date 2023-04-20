13dd6a9b0e5a4a665d995616cb8d89450b3ab308
Rodrigo Limões <rolimoes@gmail.com>
2023-04-20 09:22:37 -0300

    feat: Export the Rating repository

--- src/rating/index.ts
+++ src/rating/index.ts
@@ -3,6 +3,6 @@ import { Rating } from '@rating/entity/rating.entity';
 import { RatingController } from '@rating/rating.controller';
 import { RatingService } from '@rating/rating.service';
 
-const repository = dataSource.getRepository(Rating);
+export const repository = dataSource.getRepository(Rating);
 export const ratingService = new RatingService(repository);
 export const ratingController = new RatingController(ratingService);
